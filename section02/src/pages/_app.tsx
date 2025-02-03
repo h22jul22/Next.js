f9f1c7a9f7100291622efeae5f8eaed9abe65704
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-02-03 19:04:43 +0900

    네비게이팅

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    const router = useRouter();
+
+    const moveToTestPage = () => {
+        router.push('/test');
+    };
+    return (
+        <>
+            <header>
+                <Link href={'/'}>index</Link>
+                <Link href={'/search'}>search</Link>
+                <Link href={'/book/1'}>book/1</Link>
+                <div>
+                    <button onClick={moveToTestPage}>test 페이지로 이동</button>
+                </div>
+            </header>
+            <Component {...pageProps} />
+        </>
+    );
 }
