480a64c310c138cfec1fcb7ec5bf0a13e17153e6
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-02-04 14:10:35 +0900

    prefetchig

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
@@ -9,11 +10,19 @@ export default function App({ Component, pageProps }: AppProps) {
     const moveToTestPage = () => {
         router.push('/test');
     };
+
+    // 프로그레매틱하게 이동하는 페이지 명시적 prefetching
+    useEffect(() => {
+        router.prefetch('/test');
+    }, []);
+
     return (
         <>
             <header>
                 <Link href={'/'}>index</Link>
-                <Link href={'/search'}>search</Link>
+                <Link href={'/search'} prefetch={false}>
+                    search
+                </Link>
                 <Link href={'/book/1'}>book/1</Link>
                 <div>
                     <button onClick={moveToTestPage}>test 페이지로 이동</button>
