e468b01f6972f55f1d06c1b64b946a2fa5fdf584
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-02-10 15:01:08 +0900

    데이터 페칭

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -1,22 +1,53 @@
-import BookItem from "@/components/book-item";
-import style from "./page.module.css";
-import books from "@/mock/books.json";
+import BookItem from '@/components/book-item';
+import style from './page.module.css';
+import books from '@/mock/books.json';
+import { BookData } from '@/types';
+
+async function AllBooks() {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+        const allBooks: BookData[] = await response.json();
+        return (
+            <div>
+                {allBooks.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </div>
+        );
+    } catch (error) {
+        console.error(error);
+        return <div>오류가 발생했습니다...</div>;
+    }
+}
+
+async function RecoBooks() {
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+        const recoBooks: BookData[] = await response.json();
+        return (
+            <div>
+                {recoBooks.map((book) => (
+                    <BookItem key={book.id} {...book} />
+                ))}
+            </div>
+        );
+    } catch (error) {
+        console.error(error);
+        return <div>오류가 발생했습니다...</div>;
+    }
+}
 
 export default function Home() {
-  return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
-  );
+    return (
+        <div className={style.container}>
+            <section>
+                <h3>지금 추천하는 도서</h3>
+                <RecoBooks />
+            </section>
+            <section>
+                <h3>등록된 모든 도서</h3>
+                <AllBooks />
+            </section>
+        </div>
+    );
 }
