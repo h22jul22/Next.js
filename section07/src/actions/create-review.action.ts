f5908896190c39e73fc434c785ffd9482138ee81
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-02-13 14:26:59 +0900

    클라이언트 컴포넌트에서의 서버액션

--- section07/src/actions/create-review.action.ts
+++ section07/src/actions/create-review.action.ts
@@ -2,24 +2,39 @@
 
 import { revalidateTag } from 'next/cache';
 
-export async function createReviewAction(formData: FormData) {
+// ReviewEditor 컴포넌트에서 useActionState를 통해서 감싸서 사용하게 되면 매게변수로 첫번째로 state를 받아와야 됨.
+// (state: any, formData: FormData) -> state 값 활용하지 않기때문에 _언더스코어 활용
+export async function createReviewAction(_: any, formData: FormData) {
     const bookId = formData.get('bookId')?.toString();
     const content = formData.get('content')?.toString();
     const author = formData.get('author')?.toString();
 
-    if (!content || !author) return;
+    if (!content || !author) {
+        return {
+            status: false,
+            error: '리뷰 내용과 작성자를 입력해주세요.',
+        };
+    }
 
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`, {
             method: 'POST',
             body: JSON.stringify({ bookId, content, author }),
         });
-        console.log(response.status);
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
 
         revalidateTag(`review-${bookId}`);
+        return {
+            status: true,
+            error: '',
+        };
     } catch (error) {
-        console.error(error);
-        return;
+        return {
+            status: false,
+            error: '리뷰 저장에 실패했습니다.',
+        };
     }
 }
 
